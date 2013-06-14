c9e4d472ab3e50131df60bea93f24560a8eeb7e3
Jon Bates <jon.bates@justgiving.com>
2013-06-14 17:29:16 +0100

    Added some helpers to make reflection a little more fluent

--- SimpleConfig/Helpers/TypeHelper.cs
+++ SimpleConfig/Helpers/TypeHelper.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Xml;
 
 namespace SimpleConfig.Helpers
 {
@@ -25,7 +24,7 @@ namespace SimpleConfig.Helpers
 
         public static bool IsComplex(this Type @this)
         {
-            return @this.IsClass || @this != typeof(string);
+            return @this.IsClass && @this != typeof(string);
         }
 
         public static bool IsEnumerable(this Type @this)
@@ -35,27 +34,6 @@ namespace SimpleConfig.Helpers
                     typeof (IEnumerable),
                     typeof (IEnumerable<>)
                 }.Any(x => x.IsAssignableFrom(@this));
-        }
-    }
-
-    public static class XmlElementHelper
-    {
-        public static string GetAttributeValue(this XmlElement @this, string attributeName)
-        {
-            var result = @this.Attributes
-                              .Cast<XmlAttribute>()
-                              .FirstOrDefault(x => x.Name.Equals(attributeName, StringComparison.OrdinalIgnoreCase));
-
-            return result == null ? null : result.Value;
-        }
-
-        public static XmlElement GetElementNamed(this XmlElement @this, string elementName)
-        {
-            var result = @this.ChildNodes
-                              .OfType<XmlAttribute>()
-                              .FirstOrDefault(x => x.Name.Equals(elementName, StringComparison.OrdinalIgnoreCase));
-
-            return result;
         }
     }
 }
---
+++ SimpleConfig/Helpers/XmlElementHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+
+namespace SimpleConfig.Helpers
+{
+    public static class XmlElementHelper
+    {
+        public static string GetAttributeValue(this XmlElement @this, string attributeName)
+        {
+            var result = @this.Attributes
+                              .Cast<XmlAttribute>()
+                              .FirstOrDefault(x => x.Name.Equals(attributeName, StringComparison.OrdinalIgnoreCase));
+
+            return result == null ? null : result.Value;
+        }
+
+        public static XmlElement GetElementNamed(this XmlElement @this, string elementName)
+        {
+            var result = @this.ChildNodes
+                              .OfType<XmlElement>()
+                              .FirstOrDefault(x => x.Name.Equals(elementName, StringComparison.OrdinalIgnoreCase));
+
+            return result;
+        }
+    }
+}
